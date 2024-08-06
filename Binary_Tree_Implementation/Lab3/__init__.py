6d0b313bd60d7ccd80f9d3614c65b0eb4e6ce366
pjsul <pjsulliv34@gmail.com>
2024-08-06 12:27:55 -0700

    update

--- Binary_Tree_Implementation/Lab3/__init__.py
+++ Binary_Tree_Implementation/Lab3/__init__.py
@@ -1,2 +1,2 @@
 # This function willl be imported every time the package is imported.
-from Lab2.lab2_functions import process_input_file
+from Lab3.lab3_functions import process_main
--- Binary_Tree_Implementation/Lab3/huffman.py
+++ Binary_Tree_Implementation/Lab3/huffman.py
@@ -1,209 +1,117 @@
+# Create Priority Queue
+from Lab3.tree_node import TreeSearch
 
-from Lab2.operand import Operands
-class Expressions:
-    """
-    The Expression class enables users to convert from Postfix to Prefix and Infix and Prefix to Postfix and Infix. We currently do 
-    not have infix to postfix and prefix, but that can be a future enhancement. The Expression object takes in a Postfix, Prefix or Infix 
-    as a string. Then we have three main methods, to postfix, to prefix and to infix. These methods, use the input type to verify the process 
-    required in order to convert to the desired expression. Once the process has been confirmed, then the method calls on one of the four 
-    methods to convert from the input type to the desired output type. The last method in the class is the reverse order method. This method 
-    takes in a string and reverses the order of the string recursively processing each item in the string.
-    """
+class Huffman:
 
-    # Initialize class instance variables
-    def __init__(self, string, input_type):
-        self.string = string
-        self.input_type = input_type
-        self.valid_types = ['Prefix','Postfix','Infix']
-        # This is a method to notify the user if they entred an incorrect input type when creating the Expression Object
-        if self.input_type not in self.valid_types:
-            print(f"The type you entered: {input_type}, is incorrect. Please enter a type from the following: {self.valid_types}")
-
-
-    # This Method uses the inputy type to select the correct method in order to convert from the input type to the desired output type.
-    def to_post_fix(self):
-        if self.input_type == 'Prefix':
-            return self.pre_to_post()
-        elif self.input_type == 'Infix':
-            return "Process does not exist at the moment! Try again another time."
+    def build_Huffman_tree(self, sorted_nodes):
+        while len(sorted_nodes) >1:
+            left = sorted_nodes.pop()
+            right = sorted_nodes.pop()
+            val = left.value + right.value
+            char = left.char + right.char
+            parent_node = TreeSearch(val, char, left, right )  
+            sorted_nodes.append(parent_node)
+            sorted_nodes = self.sort_priority(sorted_nodes)      
+        return sorted_nodes[0]
+  
+    def get_huffman_codes(self,huffman_tree, huffman_list = [], prefix = ''):
+        if huffman_tree.left is None and huffman_tree.right is None:
+            huffman_list.append([huffman_tree.char,prefix])
         else:
-            return self.string
-
-    # This Method uses the inputy type to select the correct method in order to convert from the input type to the desired output type.    
-    def to_pre_fix(self):
-        if self.input_type == 'Postfix':       
-            return self.post_to_pre()
-        elif self.input_type == 'Infix':
-            return "Process does not exist at the moment. Try again another time."
-        else:
-            return self.string
-        
-    # This Method uses the inputy type to select the correct method in order to convert from the input type to the desired output type.
-    def to_infix(self):
-        if self.input_type =='Prefix':
-           return self.pre_to_infix()
-        elif self.input_type == 'Postfix':
-           return self.post_to_infix()
-        else:
-            return self.string
-
-    # Initialize a list of operators that is used in the methods below.         
-    operators = ['+','-','/','*','$']
-
-    # This Method converts from prefix to post fix
-    def pre_to_post(self, postfix = None, index = 0):
-
-        # Check to see if a postfix list has been inputed into the function
-        if postfix == None:
-
-            # Initialize Empty list
-            postfix = []
-
-        # Base Case of recursive function. 
-        if index >= len(self.string):
-
-            # If clause to check if length of postfix list is greater than 1, if so, return error, else return final element in list
-            if len(postfix) >1:
-                return 'Incorrect Number of Operators'
-            else:
-                final_item = postfix.pop()
-                return final_item
-            
-        # Grab current element in recursive call based on inputed index
-        # List is reverse before traversing to grab element    
-        char = self.reverse_order(self.string)[index]
-
-        # Check to see if char in the class variable operators
-        if char not in Expressions.operators:
-
-            # If not add char to postfix list and increment index
-            postfix.append(char)
-            index +=1
-
-            # Recursive call to pre_to_post function
-            return self.pre_to_post(postfix, index)
-        
-        # Else clause assumes character not in class variable operators
-        else:
-
-            # Check to see if the postfix list is less than 2 elements, if so, return error
-            if len(postfix) <2:
-                return "Incorrect Number of Operands"
-            else:
-
-                # Use pop function to grab the first two elements in postfix list
-                item1 = postfix.pop()
-                item2 = postfix.pop()
-
-                # Create an operand object using the operand class
-                operand = Operands(item1,item2,char,'Pre_Post')
-
-                # Using operand method, append list with new opperand. Increment index by 1.
-                postfix.append(operand.combine_operands())
-                index +=1
-
-                # Recursive call to pre_to_post function
-                return self.pre_to_post(postfix, index)
-
-     
-
-    # This Method Converts from Postfix to Prefix
-    def post_to_pre(self, prefix = None, index = 0):
-        if prefix == None:
-            prefix = []
-        if index >= len(self.string):
-            if len(prefix) >1:
-                return 'Incorrect Number of Operators'
-            else:
-                final_item = prefix.pop()
-
-                # reverse the string before returning the final prefix string.
-                return self.reverse_order(final_item)
-        char = self.string[index]
-        if char not in Expressions.operators:
-            prefix.append(char)
-            index +=1
-            return self.post_to_pre(prefix, index)
-        else:
-            if len(prefix) <2:
-                return "Incorrect Number of Operands"
-            else:
-                item1 = prefix.pop()
-                item2 = prefix.pop()
-                operand = Operands(item1,item2,char,'Post_Pre')
-                prefix.append(operand.combine_operands())
-                index +=1
-                return self.post_to_pre(prefix, index)
-
-
-    # This methood converts from prefix to infix
-    def pre_to_infix(self, infix = None, index = 0):
-        if infix == None:
-            infix = []
-        if index >= len(self.string):
-            if len(infix) >1:
-                return 'Incorrect Number of Operators'
-            else:
-                final_item = infix.pop()
-                return final_item
-        char = self.reverse_order(self.string)[index]
-        if char not in Expressions.operators:
-            infix.append(char)
-            index +=1
-            return self.pre_to_infix(infix, index)
-        else:
-            if len(infix) <2:
-                return "Incorrect Number of Operands"
-            else:
-                item1 = infix.pop()
-                item2 = infix.pop()
-                operand = Operands(item1,item2,char,'Pre_Infix')
-                infix.append(operand.combine_operands())
-                index +=1
-                return self.pre_to_infix(infix, index)
-        
-
-    # This Method converts from postfix to infix
-    def post_to_infix(self, infix = None, index = 0):
-        if infix == None:
-            infix = []
-        if index >= len(self.string):
-            if len(infix) >1:
-                return 'Incorrect Number of Operators'
-            else:
-                final_item = infix.pop()
-                return final_item
-        char = self.string[index]
-        if char not in Expressions.operators:
-            infix.append(char)
-            index +=1
-            return self.post_to_infix(infix, index)
-        else:
-            if len(infix) <2:
-                return "Incorrect Number of Operands"
-            else:
-                item1 = infix.pop()
-                item2 = infix.pop()
-                operand = Operands(item1,item2,char,'Post_Infix')
-                infix.append(operand.combine_operands())
-                index +=1
-                return self.post_to_infix(infix, index)
-            
-    # This Method takes a string and reverses the order of the string.    
-    def reverse_order(self, string):
-
-        # Base Case of recursive function
-        if len(string) == 0:
-            return string  
-
-        # Recursive call grabbing last element and string to the last element inclusive 
-        return string[-1] + self.reverse_order(string[:-1])
-
-
-       
-
+            #print(f'{huffman_tree.left.value} left, {huffman_tree.right.value} right')
+            if huffman_tree.left is not None:
+                self.get_huffman_codes(huffman_tree.left,  huffman_list , prefix +'0')
+            if huffman_tree.right is not None:
+                self.get_huffman_codes(huffman_tree.right,  huffman_list , prefix +'1')
+        return  huffman_list
     
+    def sort_priority(self,list_):
+        stack = []
+        alpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+        while list_:
+            current = list_.pop()
+            # Insert current element into the correct position in the stack
+            temp_stack = []
+            current_index = 0
+            current_stack_index = 0
+            for i in alpha:
+                if current.char == alpha[current_index]:
+                    break
+                else:
+                    current_index +=1
+            for i in alpha:
+                if stack and stack[-1].char == alpha[current_stack_index]:
+                    break
+                else:
+                    current_stack_index+=1
+            while stack and (stack[-1].value < current.value or 
+                            (stack[-1].value == current.value and len(stack[-1].char) < len(current.char)) or
+                            (stack[-1].value == current.value and len(stack[-1].char) == len(current.char) and current_stack_index < current_index)):
+                temp_stack.append(stack.pop())
+            
+            stack.append(current)
+            
+            # Push back elements from the temporary stack to the main stack
+            while temp_stack:
+                stack.append(temp_stack.pop())
+        
+        return stack
+        
+    
+    def encode_huffman( self, string, huffman_code):
+        final = ''
+        
+        
+        for c in string:
+            
+            for code in huffman_code:
+                
+                if c.lower() == code[0].lower():
+                    final += code[1]
+                    break  
+       
+        return final
+    
+    def decode_huffman(self,binary_string, huffman_code):
+        final = ''
+        temp_char = ''
+        for c in binary_string:
+            temp_char += c
+            for code in huffman_code:
+                if temp_char == code[1]:
+                    final += code[0]
+                    temp_char = ''
+                    break  
+        return final
+    
+    def preOrderTraversal(self,node, output = None):
+        if output is None:
+            output = []
+        if node is None:
+            return output
         
 
+        # Deal with the node
+        print(f'{node.value} : {node.char}')
+        output.append([node.value,node.char])
+
+        # Recur on left subtree
+        self.preOrderTraversal(node.left, output)
+
+        # Recur on right subtree
+        self.preOrderTraversal(node.right, output)
+
+        return output
+
+        
+        
     
    
+
+   
+
+
+    
+
+
+
