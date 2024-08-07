37cea5564e6369de18487e36937be889e08cc090
pjsul <pjsulliv34@gmail.com>
2024-08-06 20:54:50 -0700

    added in test cases

--- Binary_Tree_Implementation/Lab3/lab3_functions.py
+++ Binary_Tree_Implementation/Lab3/lab3_functions.py
@@ -1,75 +1,80 @@
-# Import Expressions class
-#from Lab2.expressions import Expressions
-from Lab3.tree_node import TreeSearch
+# Import Huffman Class
 from Lab3.huffman import Huffman
 
-# This method will process the input file based on input type and desired output type and then output
-# the data into the output file at the desired location
+# This method will process frequency table, encoded data, and clear text data.
+# After processing the data, the processed data will be writen to the specified outpath.
+def process_main(freq_type,encoded,clear_text,outpath):
 
-
-def process_main(freq_type,encoded,clear_text,outpath,test_case):
+    # Read in Data
     freq_table = process_files(freq_type,'freq')
-    print(freq_table)
     encoded = process_files(encoded,'encoded')
-    print('encoded')
-    print(encoded)
-    print(len(encoded))
     clear_text = process_files(clear_text,'clear')
-    print(clear_text)
-
-
-    tree_list = []
-    for value in freq_table:
-        tree_list.append(TreeSearch(value[1],value[0]))
-        
-    
-    huffman_ob = Huffman()
-    reverse_sorted = huffman_ob.sort_priority(tree_list)
-    print(reverse_sorted)
    
+    # Initialize an Empty list
+    tree_list = []
+
+    # Loop through frequency table creating a TreeNode for each number/letter combination
+    for value in freq_table:
+        tree_list.append(Huffman(value[1],value[0]))
+        
+    # Initialize an Empty Tree Node
+    huffman_ob = Huffman(None,None)
+
+    # Set up Priority Queue for Tree Node list
+    reverse_sorted = huffman_ob.sort_priority(tree_list)
+    
+    # Build Huffman tree utilizing Huffman method
     huffman_tree = huffman_ob.build_Huffman_tree(reverse_sorted)
-    print(huffman_tree)
-
-    
+  
+    # Create a list object that represents the pre order traversal of the huffman tree
     pre_order_traversal = huffman_ob.preOrderTraversal(huffman_tree)
-    print(pre_order_traversal)
-    
+  
+    # Grab the huffman codes from Huffman tree
     huffman_code = huffman_ob.get_huffman_codes(huffman_tree,huffman_list=[])
-    print(huffman_code)
+   
+    # Open the Outpath file in writer mode
+    with open(f'{outpath}','w') as writer:
 
-    
-
-    with open(f'{outpath}.txt','w') as writer:
-
-        print(f'Test Scenario : {test_case}')
-
-        # print('PreOrder Traversal of Huffman Tree')
         writer.write('PreOrder Traversal of Huffman Tree\n')
-        print(f'len of pre {len(pre_order_traversal)}')
+        
+        # Loop through each line in the Pre Order Traversal of the Huffman tree
         for line in pre_order_traversal:
-            print(line)
             writer.write(f'{line}\n')
 
+
+        # Decoding Section
         writer.write(f'\nDecoding Messages----------\n')
 
+        # Loop through encoded messages in Encoded file
         for encoded_message in encoded:
-            decoded = huffman_ob.decode_huffman(encoded_message,huffman_code)
-            print(decoded)
-            writer.write(f"Encodings  \n input: {encoded_message}\n output: {decoded}\n")
 
-        writer.write(f'\nEncoding Messages\n')
+            # Decode the ecoded message using the Huffman code
+            try:
+                decoded = huffman_ob.decode_huffman(encoded_message,huffman_code)
+                writer.write(f"Encodings  \n input: {encoded_message}\n output: {decoded}\n")
+            except Exception as e:
+                print(e)
+
+
+        # Encoding Section
+        writer.write(f'\nEncoding Messages----------\n')
         for clear in clear_text:
-            print('clear')
-            print(clear)
-            encoded = huffman_ob.encode_huffman(clear,huffman_code )
-            writer.write(f"Decodings  \n input: {clear}\n output: {encoded}\n")
+            
+            try:
+                encoded = huffman_ob.encode_huffman(clear,huffman_code )
+                writer.write(f"Decodings  \n input: {clear}\n output: {encoded}\n")
+            except Exception as e:
+                print(e)
     
     
    
     
-    
+# Function to read in file based on file type
 def process_files(input,type):
+
+    # Initialize empty list
     processed_data = []
+
     # Open up the input file in reader mode
     with open(input,'r') as reader:
 
@@ -78,9 +83,7 @@ def process_files(input,type):
 
             # Read in the first line and strip white space from the right and left
             line = reader.readline().strip()
-            print(line)
-            
-
+        
             # Replace any white space in between operators and characters with emtpy string
             line = line.replace(' ', '')
 
@@ -91,15 +94,17 @@ def process_files(input,type):
             # Try clause to process the line in the input file
             if type =='freq':
                 try:
-                    #print(line)
                     split = line.split('-')
                     processed_data.append([split[0],int(split[1])])
                 except Exception as e:
                     print(e)
-            elif type == 'encoded':
-                processed_data.append(line)
+            elif type in ('encoded','clear'):
+                try:
+                    processed_data.append(line)
+                except Exception as e:
+                    print(e)
             else:
-                processed_data.append(line)
+                print('Unknown File')
 
         return processed_data
            
